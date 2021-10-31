fdc2f5f9eb87c09f7ea219e5854a2b1c4626f0de
Anony 31 <d0gey@users.noreply.github.com>
2021-10-31 17:10:33 -0300

    Adiciona método para buscar CEP a partir de um endereço

--- cep.ts
+++ cep.ts
@@ -15,3 +15,8 @@ export async function getCepInfo(cep: string): Promise<CepInfo> {
   const response = await fetch(`https:/1/viacep.com.br/ws/${cep}/json/`);
   return response.json();
 }
+
+export async function obterCepEndereço(estado: string, cidade: string, rua: string): Promise<CepInfo[]> {
+  const response = await fetch(`https://viacep.com.br/ws/${estado}/${cidade}/${rua}/json/`);
+  return response.json();
+}
